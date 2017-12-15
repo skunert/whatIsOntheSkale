d7b80ed972820b5078db1af7d2bdc92209f22707
Sebastian Kunert <sebastian.kunert@codecentric.de>
2017-12-15 14:00:32 +0100

    Modified config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   //user id is needed when you send messages to connected devices
   //replace this string with your user id, check README.md how to do it.
-  userId: '559d0faf-09de-4e69-a150-73acb412906b',
+  userId: '14634b94-bfdf-4a21-b220-109fbfc492db',
 
   //this is the url for sending the messages to connected devices,
   //is provided from the runtime environment, do not modify this
@@ -22,10 +22,15 @@ module.exports = {
   //make sure you include only the message type ids that your application is enabled
   //check README.md for more details
   //do not modify the names, if you are not sure
+  /* subscribeMessageTypes: {
+     'a2d38aa9-cc72-4790-8554-ffbd11925cb6': 'skaleMeasure',
+     '8a41092a-4a07-461d-9274-8409f60ac7e4': 'skaleButton',
+     '54121087-14f1-4c2a-835f-117681618cc9': 'develco'
+   },*/
+
   subscribeMessageTypes: {
     'a2d38aa9-cc72-4790-8554-ffbd11925cb6': 'skaleMeasure',
     '8a41092a-4a07-461d-9274-8409f60ac7e4': 'skaleButton',
-    '54121087-14f1-4c2a-835f-117681618cc9': 'develco'
   },
 
   //this defines the message store structure
@@ -40,7 +45,7 @@ module.exports = {
       items: [],
       limit: 100
     },
-    develco: {
+    skaleCommand: {
       items: [],
       limit: 100
     },
@@ -54,20 +59,15 @@ module.exports = {
   //this is useful when sending commands the connected devices
   // check README.md for details
   things: {
-    smart_plug_1: {
-      thingId: 'b14a4a0c-203d-42da-8005-423682eabc17',
-      messageTypeId: '5d610b4f-5532-4e24-b46f-c355bdd99958',
-      cmd_on: 'on',
-      cmd_off: 'off'
-    },
-    smart_bulb_1: {
-      thingId: '9bc506fa-218c-469a-8149-230107410825',
-      messageTypeId: '5d610b4f-5532-4e24-b46f-c355bdd99958',
-      cmd_on: 'on',
-      cmd_off: 'off'
-    },
-    skale_1: {
-      thingId: 'dc5eb2e9-3ae3-448d-8ced-80c23ae2b2cd',
+    /*TheSkale: { 
+      id: "c6cf0125-ba53-4e94-aad1-1e112e610895", 
+      "name": "TheSkale", 
+      "serial_number": "E8196B03-5B06-4700-B6A7-460E1480C24F", 
+      "thing_type": "a4526b26-cc04-4205-a8d3-c4a8dab1389d", 
+      "created": "2017-12-15T10:31:02.284Z" 
+    },*/
+    TheSkale: {
+      thingId: 'c6cf0125-ba53-4e94-aad1-1e112e610895',
       messageTypeId: 'fc7a9697-f98e-4f51-8b23-fd1b8b8f02e1',
       cmd_led_on: 0xED,
       cmd_led_off: 0xEE,
